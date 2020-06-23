4748dd4481c9acb73a5051d96f5d8a19d07aaf4b
Italo <DarkGhostHunter@Gmail.com>
2020-06-23 02:36:12 -0400

    Allows additional headers on Login Action.

--- src/useLogin.js
+++ src/useLogin.js
@@ -4,14 +4,14 @@ import {
     preparePublicKeyOptions,
 } from './common';
 
-const useLogin = ({actionUrl = '/login', optionsUrl = '/login/options'}) => {
+const useLogin = ({actionUrl = '/login', optionsUrl = '/login/options', headers = {}}) => {
     return async (data) => {
         const optionsResponse = await fetchEndpoint(data, optionsUrl);
         const json = await optionsResponse.json();
         const publicKey = preparePublicKeyOptions(json);
         const credentials = await navigator.credentials.get({publicKey});
         const publicKeyCredential = preparePublicKeyCredentials(credentials);
-        const actionResponse = await fetchEndpoint(publicKeyCredential, actionUrl);
+        const actionResponse = await fetchEndpoint(publicKeyCredential, actionUrl, headers);
 
         return await actionResponse.json();
     };
