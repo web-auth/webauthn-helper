d605c22be2064828ed9363a39517dc702a6aed90
Tsjippy <44447654+Tsjippy@users.noreply.github.com>
2021-12-10 16:31:07 +0100

    Update useLogin.js
    better logging and optional callback

--- src/useLogin.js
+++ src/useLogin.js
@@ -4,18 +4,25 @@ import {
     preparePublicKeyOptions,
 } from './common';
 
-const useLogin = ({actionUrl = '/login', actionHeader = {}, optionsUrl = '/login/options'}, optionsHeader = {}) => {
+const useLogin = ({actionUrl = '/login', actionHeader = {}, optionsUrl = '/login/options', optionsHeader = {}, afterOptionsSend=null}) => {
     return async (data) => {
         const optionsResponse = await fetchEndpoint(data, optionsUrl, optionsHeader);
         const json = await optionsResponse.json();
+        if (! optionsResponse.ok) {
+            throw json;
+        }
         const publicKey = preparePublicKeyOptions(json);
         const credentials = await navigator.credentials.get({publicKey});
+        
+        //callback before verifying authentication
+        if(typeof(afterOptionsSend)=='function'){afterOptionsSend();}
+   
         const publicKeyCredential = preparePublicKeyCredentials(credentials);
         const actionResponse = await fetchEndpoint(publicKeyCredential, actionUrl, actionHeader);
-        if (! actionResponse.ok) {
-            throw actionResponse;
-        }
         const responseBody = await actionResponse.text();
+        if (! actionResponse.ok) {
+            throw responseBody;
+        }
 
         return responseBody !== '' ? JSON.parse(responseBody) : responseBody;
     };
